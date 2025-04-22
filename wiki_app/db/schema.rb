a3f02b94b2a863ca816d9cfabd6dbf54476ab756
Spiffai <spiffai@gmail.com>
2025-04-22 15:08:59 -0400

    updated page show and listings w schema changes

--- wiki_app/db/schema.rb
+++ wiki_app/db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_22_183742) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_22_185520) do
   create_table "wiki_posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
