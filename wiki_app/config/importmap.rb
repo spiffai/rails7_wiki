c0beb494e86cbc2e6bef9298c0f36631a38bc0d6
Spiffai <spiffai@gmail.com>
2025-04-23 10:50:57 -0400

    tinymce and form updates

--- wiki_app/config/importmap.rb
+++ wiki_app/config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# TinyMCE
+pin "tinymce", to: "tinymce/tinymce.min.js"
+pin_all_from "tinymce", under: "tinymce", to: "tinymce"
